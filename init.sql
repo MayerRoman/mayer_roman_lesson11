2f0768846bcd65e66ab5e706a3347a0411f3c300
Roman Mayer <mayerroman@mail.ru>
2016-06-10 11:59:25 +0400

    initial commit

---
+++ init.sql
@@ -0,0 +1,71 @@
+﻿CREATE TABLE IF NOT EXISTS label (
+id BIGINT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+director VARCHAR(255) NOT NULL,
+country VARCHAR(255) NOT NULL,
+rating SMALLINT CHECK (rating > 0 AND rating < 6));
+
+INSERT INTO label
+VALUES 
+(1, 'Island Records', 'Asif Kapadia', 'United Kingdom', 5),
+(2, 'A&M Records', 'Herb Alpert', 'United States', 5),
+(3, 'Albert Productions', 'Ted Albert', 'Australia', 4),
+(4, 'Parlophone ', 'Carl Lindström', 'United Kingdom', 5),
+(5, 'Universal Pictures Music', 'Lucian Grainge', 'United Kingdom', 3);
+
+
+
+CREATE TABLE IF NOT EXISTS singer (
+id BIGINT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+concert_price INTEGER CHECK (concert_price > 0),
+label_id BIGINT,
+FOREIGN KEY (label_id) REFERENCES label (id) ON UPDATE CASCADE);
+
+INSERT INTO singer
+VALUES 
+(1, 'U2', 1500000, 1),
+(2, 'Sting', 1000000, 2),
+(3, 'AC/DC', 1100000, 3),
+(4, 'Coldplay', 1150000, 4),
+(5, 'Billy Mack', 500000, 1);
+
+
+
+CREATE TABLE IF NOT EXISTS album (
+id BIGINT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+genre VARCHAR(255) NOT NULL,
+singer_id BIGINT NOT NULL,
+FOREIGN KEY (singer_id) REFERENCES singer (id) ON UPDATE CASCADE);
+
+INSERT INTO album
+VALUES 
+(1, 'All That You Cant Leave Behind', 'POP', 1),
+(2, 'No Line On The Horizon', 'ROCK', 1),
+(3, 'Brand New Day', 'POP', 2),
+(4, 'Fields Of Gold', 'POP', 2),
+(5, 'Iron Man 2', 'ROCK', 3),
+(6, 'A Head Full Of Dreams', 'ROCK', 4);
+
+
+
+CREATE TABLE IF NOT EXISTS song (
+id BIGINT PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+duration INTERVAL NOT NULL,
+singer_id BIGINT NOT NULL,
+album_id BIGINT,
+FOREIGN KEY (singer_id) REFERENCES singer (id) ON UPDATE CASCADE,
+FOREIGN KEY (album_id) REFERENCES album (id) ON UPDATE CASCADE);
+
+INSERT INTO song
+VALUES 
+(1, 'Beautiful Day', INTERVAL '00:04:15', 1, 1),
+(2, 'Walk On', INTERVAL '00:03:37', 1, 1),
+(3, 'No Line On The Horizon', INTERVAL '00:04:07', 1, 2),
+(4, 'Desert Rose', INTERVAL '00:03:25', 2, 3),
+(5, 'Fragile', INTERVAL '00:05:01', 2, 4),
+(6, 'Back In Black', INTERVAL '00:03:54', 3, 5),
+(7, 'Adventure Of A Lifetime', INTERVAL '00:04:14', 4, 6),
+(8, 'Christmas is all Around', INTERVAL '00:06:05', 5, null);
---
+++ query.sql
@@ -0,0 +1,49 @@
+﻿/*Получить название и длительность песен каждого исполнителя*/
+SELECT si.name, s.title, s.duration
+FROM singer si
+JOIN song s ON si.id = s.singer_id
+ORDER BY 1;
+
+/*Получить стоимость концерта на копоративе*/
+SELECT SUM(concert_price)
+FROM singer
+WHERE name IN ('AC/DC', 'Sting', 'Billy Mack');
+
+/*Получить длительность песен в каждом альбоме каждого исполнителя*/
+SELECT si.name singer, a.name album, SUM(s.duration) "total duration"
+FROM singer si
+JOIN album a
+ON si.id = a.singer_id
+JOIN song s
+ON a.id = s.album_id
+GROUP BY 1, 2
+ORDER BY singer, album;
+
+
+/*Получить песни, не вошедшие ни в один альбом*/
+SELECT title
+FROM song
+WHERE album_id is NULL;
+
+/*Получить все лейблы с рейтингом 5, расположенные в Великобритании*/
+SELECT name
+FROM label
+WHERE rating = 5
+AND country = 'United Kingdom';
+
+/*Получить названия лейблов и относящиеся к ним песни*/
+SELECT l.name, s.title
+FROM label l
+JOIN singer si
+ON l.id = si.label_id
+JOIN song s
+ON si.id = s.singer_id
+ORDER BY 1;
+
+/*Получить суммарную продолжительность песен каждого исполнителя*/
+SELECT si.name, SUM(s.duration)
+FROM singer si
+JOIN song s
+ON si.id = s.singer_id
+GROUP BY 1
+ORDER BY 2 DESC;
